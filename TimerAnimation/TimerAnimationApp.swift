9ce3111654fa5376fa820c9cad13f93286ae978f
alucchini <antoine.lucchini@ynov.com>
2023-03-24 10:27:46 +0100

    Initial Commit

---
+++ TimerAnimation/TimerAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAnimationApp.swift
+//  TimerAnimation
+//
+//  Created by Antoine Lucchini on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimerAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
